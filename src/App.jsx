7a8efa8ba9032493bad9565545624217fcd19f0b
Lena <petukh01@ads.uni-passau.de>
2026-02-03 20:16:08 +0100

    move orders page to react

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
 import { Routes, Route } from 'react-router';
 import { HomePage } from './pages/HomePage';
 import { CheckoutPage } from './pages/CheckoutPage';
+import { OrdersPage } from './pages/OrdersPage';
 
-import './App.css'
+import './App.css';
 
 function App() {
 
   return (
     //all pages of the web-site  
     <Routes>  
+
       {/* path - at the url, element - the component to display -  */}
       {/* index means "/", main page */}
-      <Route index element = {<HomePage />} />
-      <Route path = "checkout" element = {<CheckoutPage />} />
+      <Route index element = { <HomePage /> } />
+      <Route path = "checkout" element = { <CheckoutPage /> } />
+      <Route path = "orders" element = { <OrdersPage /> } />
       
     </Routes>
     
-  )
+  );
 }
 
-export default App
+export default App;
