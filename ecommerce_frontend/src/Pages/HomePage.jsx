ac2043633d25365fe3046444836dee749f2332dc
Lena <petukh01@ads.uni-passau.de>
2026-02-05 14:00:39 +0100

    add server proxy config for /api and /images

--- ecommerce_frontend/src/Pages/HomePage.jsx
+++ ecommerce_frontend/src/Pages/HomePage.jsx
@@ -16,13 +16,13 @@ export function HomePage() {
     //useEffect let us control when the code run. default - every time the component is created 
     //or updated 
     useEffect(() => {
-        axios.get('http://localhost:3000/api/products')
+        axios.get('/api/products')
             .then((response) => {
                 setProducts(response.data);
             });
         
         //get number of items in cart 
-        axios.get('http://localhost:3000/api/cart-items')
+        axios.get('/api/cart-items')
         .then((response) => {
             setCart(response.data);
         });
