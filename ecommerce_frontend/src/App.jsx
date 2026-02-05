e804e24230aa64e6479f6871e70c01b6cca7583c
Lena <petukh01@ads.uni-passau.de>
2026-02-05 14:16:00 +0100

    lift the cart up and share it between pages

--- ecommerce_frontend/src/App.jsx
+++ ecommerce_frontend/src/App.jsx
@@ -1,4 +1,6 @@
+import axios from 'axios';
 import { Routes, Route } from 'react-router';
+import { useState, useEffect } from 'react';
 import { HomePage } from './pages/HomePage';
 import { CheckoutPage } from './pages/CheckoutPage';
 import { OrdersPage } from './pages/OrdersPage';
@@ -6,21 +8,34 @@ import { TrackingPage } from './pages/TrackingPage';
 
 import './App.css';
 
+
 function App() {
+
+  //save cart
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    //get number of items in cart 
+    axios.get('/api/cart-items')
+      .then((response) => {
+        setCart(response.data);
+      });
+  }, []) //run once 
 
   return (
     //all pages of the web-site  
-    <Routes>  
+    <Routes>
 
       {/* path - at the url, element - the component to display -  */}
       {/* index means "/", main page */}
-      <Route index element = { <HomePage /> } />
-      <Route path = "checkout" element = { <CheckoutPage /> } />
-      <Route path = "orders" element = { <OrdersPage /> } />
-      <Route path = "tracking" element = { <TrackingPage />} />
+      {/* pass cart into HomePage and into CheckoutPage via prop */}
+      <Route index element={<HomePage cart = {cart} />} />
+      <Route path="checkout" element={<CheckoutPage cart = {cart} />} />
+      <Route path="orders" element={<OrdersPage />} />
+      <Route path="tracking" element={<TrackingPage />} />
 
     </Routes>
-    
+
   );
 }
 
--- ecommerce_frontend/src/Pages/HomePage.jsx
+++ ecommerce_frontend/src/Pages/HomePage.jsx
@@ -4,14 +4,13 @@ import { useState } from 'react';
 import { Header } from '../components/Header';
 import './HomePage.css';
 
-export function HomePage() {
+export function HomePage({ cart }) {
 
     //useState - gives array with 2 values - a valiable and a function to update this value 
     //save products 
     const [products, setProducts ] = useState([]); //starting value is empty array 
 
-    //save cart
-    const [cart, setCart] = useState([]);
+
 
     //useEffect let us control when the code run. default - every time the component is created 
     //or updated 
@@ -21,11 +20,7 @@ export function HomePage() {
                 setProducts(response.data);
             });
         
-        //get number of items in cart 
-        axios.get('/api/cart-items')
-        .then((response) => {
-            setCart(response.data);
-        });
+
     }, []); // [] - dependency array, empty means the code will run once once when the component is
     //created 
     
