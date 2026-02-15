ae23d0735c53e488858b12f66c5e494c8d3efedd
Lena <petukh01@ads.uni-passau.de>
2026-02-15 09:16:31 +0100

    sending to the tracking page the cart

--- ecommerce_frontend/src/App.jsx
+++ ecommerce_frontend/src/App.jsx
@@ -39,7 +39,7 @@ function App() {
       <Route index element={<HomePage cart={cart} loadCart={loadCart} />} />
       <Route path="checkout" element={<CheckoutPage cart={cart} loadCart={loadCart}/>} />
       <Route path="orders" element={<OrdersPage cart={cart} loadCart={loadCart} />} />
-      <Route path="tracking" element={<TrackingPage />} />
+      <Route path="tracking" element={<TrackingPage cart={cart}/>} />
 
     </Routes>
 
