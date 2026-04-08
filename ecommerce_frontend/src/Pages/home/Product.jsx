d34d8d77f219a1f3cdf825ee3453378f32790da2
Lena <petukh01@ads.uni-passau.de>
2026-04-08 14:18:47 +0200

    Test clicking add to cart button

--- ecommerce_frontend/src/Pages/home/Product.jsx
+++ ecommerce_frontend/src/Pages/home/Product.jsx
@@ -87,6 +87,7 @@ export function Product({product, loadCart}) {
             </div>
             {/* onClick event handler **/}
             <button className="add-to-cart-button button-primary"
+                data-testid = "add-to-cart-button"
                 onClick={addToCart}
             >
                 Add to Cart
--- ecommerce_frontend/src/Pages/home/Product.test.jsx
+++ ecommerce_frontend/src/Pages/home/Product.test.jsx
@@ -1,6 +1,11 @@
 import { it, expect, describe, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { Product } from './Product';
+import axios from 'axios';
+
+/* fake axios */
+vi.mock('axios');
 
 
 /* Integration test suit for Product component */
@@ -48,6 +53,51 @@ describe('Product component', () => {
             screen.getByText('87')
         ).toBeInTheDocument();
         
+
+    });
+
+    /* test user interactions */
+    it('adds a product to the cart', async () => {
+
+        
+        const product = {
+            "id": "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            "image": "images/products/athletic-cotton-socks-6-pairs.jpg",
+            "name": "Black and Gray Athletic Cotton Socks - 6 Pairs",
+            "rating": {
+                "stars": 4.5,
+                "count": 87
+            },
+            "priceCents": 1090,
+            "keywords": ["socks", "sports", "apparel"]
+        };
+
+        /* mock a function */
+
+        /* vi.fn creates a fake function that doesn't do anything */
+        const loadCart = vi.fn();
+
+        render(<Product product={product} loadCart={loadCart} />);
+
+        const user = userEvent.setup();
+
+        const  addToCartButton = screen.getByTestId('add-to-cart-button');
+
+        /*async function */
+        await user.click(addToCartButton);
+
+        expect(axios.post).toHaveBeenCalledWith(
+
+            '/api/cart-items',
+            {
+                productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                quantity: 1
+            }
+
+        );
+
+        /* loadCart was called */
+        expect(loadCart).toHaveBeenCalled();
 
     });
 
